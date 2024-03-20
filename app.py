dc465e1442b82b4170ecc2d8ff03355798995b45
jananimurugesan01 <132807634+jananimurugesan01@users.noreply.github.com>
2024-03-20 16:54:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+import random
+import string
+
+app = Flask(__name__)
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    return password
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    length = int(request.form['length'])
+    password = generate_password(length)
+    return render_template('index.html', password=password)
+
+if __name__ == '__main__':
+    app.run(debug=True)
